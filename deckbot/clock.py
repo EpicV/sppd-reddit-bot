e9b0c914be9b8f0efabf24749cdac46c52fc17e2
EpicV <vincent.zyz@gmail.com>
2018-01-23 17:17:29 +1000

    run deck bot in the clock process

--- deckbot/clock.py
+++ deckbot/clock.py
@@ -5,6 +5,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     import deck_bot
+    deck_bot.main()
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
